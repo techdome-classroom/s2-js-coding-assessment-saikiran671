f56c682502587cfdabc7013d35f36c122200146c
saikiran671 <dsaikiran155@gmail.com>
2024-11-11 15:40:35 +0530

    submiting

--- program1.js
+++ program1.js
@@ -3,6 +3,24 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    const bracketMap = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+    const stack = [];
+    for (const char of s) {
+        if (bracketMap[char]) {
+            const topElement = stack.pop() || '#';
+            if (bracketMap[char] !== topElement) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
     
 };
 
